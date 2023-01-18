b0967f7e662ae2e076a29194a030d873a8fd201a
Alpro1993 <adaopinho25@gmail.com>
2023-01-18 16:45:32 +0000

    Finished Part 5

---
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models {
+    public class OfficeAssignment {
+        [Key]
+        public int InstructorID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public Instructor Instructor;
+    }
+}
--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -24,7 +24,6 @@ if (!app.Environment.IsDevelopment())
 using (var scope = app.Services.CreateScope()) {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<SchoolContext>();
-    context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
 
